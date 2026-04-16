0d571a3293c0880b09df9e2d021f0a2171800359
блазик <132152761+blaz1k@users.noreply.github.com>
2026-04-17 01:40:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,16 +3,27 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTyp
 
 TOKEN = "8619357016:AAGp4Olagb-BOuwSNJUwd5o-js2mJauG6iQ"
 
-async def reply_67(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message.text == "67":
-        await update.message.reply_text("слава 67")
-    if update.message.text == "6":
-        await update.message.reply_text("7")
+async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text.lower()
+
+    # 1) если есть "67" в любом месте текста
     if "67" in text:
-        await update.message.reply_text("ААААААААА СИКС СЕВЕЕН")
+        await update.message.reply_text("ААААА СИКС СЕВЕЕЕН 67 ПОКОЙО")
+        return
+
+    # 2) если написали просто "6" → отвечаем "7"
+    if text == "6":
+        await update.message.reply_text("7")
+        return
+
+    # 3) если написали просто "7" → отвечаем "6"
+    if text == "7":
+        await update.message.reply_text("6")
+        return
+
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply_67))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handler))
 
 app.run_polling()
